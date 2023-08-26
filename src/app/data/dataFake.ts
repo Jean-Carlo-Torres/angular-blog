d3c94634536077109fbd6a2dd30183eb7ff1e255
Jean Carlo <jeansanto2002@gmail.com>
2023-08-26 10:50:33 -0300

    Deixando o content Page dinâmica

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
    {
     "id": "1",
-    "photo": "",
-    "title": "Game developer",
-    "description": "ola"
+    "title": "Desenvolvimento de jogos",
+    "description": "O mercado de jogos é um dos que mais cresce na área de tecnologia e movimenta anualmente bilhões de dólares em todo o mundo. Se você se interessa pelo assunto e deseja ingressar nessa área como desenvolvedor de games, confira o bate-papo que tivemos sobre o assunto",
+    "photoCover": "https://user-images.githubusercontent.com/70382532/138322189-2db8df52-9dcb-40a0-88a8-c365466bd33d.gif"
    },
    {
-    "id": 2,
-    "photo": "",
-    "title": "Game developer",
-    "description": "ola"
-   }
+    "id": "2",
+    "title": "10 melhores jogos de acordo com a crítica",
+    "description": "10 melhores jogos de acordo com a crítica até a minha data de corte em setembro de 2021. Lembre-se de que a classificação dos melhores jogos pode variar de acordo com diferentes fontes e opiniões. Aqui estão alguns jogos aclamados pela crítica: The Legend of Zelda: Breath of the Wild (2017) - Nintendo Switch/Wii U, The Witcher 3: Wild Hunt (2015) - Multiplataformas, Super Mario Odyssey (2017) - Nintendo Switch, Red Dead Redemption 2 (2018) - Multiplataformas, Grand Theft Auto V (2013) - Multiplataformas, Bloodborne (2015) - PlayStation 4, The Last of Us Part II (2020) - PlayStation 4, Dark Souls (2011) - Multiplataformas, Persona 5 (2016) - Multiplataformas, Mass Effect 2 (2010) - Multiplataformas",
+    "photoCover": "https://www.gamebyte.com/wp-content/uploads/2022/05/697379c7-best-ps4-games-cover.png"
+   },
+   {
+      "id": "3",
+      "title": "Bibliotecas para .NET",
+      "description": "O .NET é uma plataforma de desenvolvedor multiplataforma de código aberto gratuita para criar muitos tipos diferentes de aplicativos.Com o .NET, você pode usar vários idiomas, editores e bibliotecas para criar para Web, dispositivos móveis, desktop, jogos, IoT e muito mais.",
+      "photoCover": "https://warpzone.me/wp-content/uploads/2020/04/th-e1586376486510-696x392.png"
+     },
+     {
+      "id": "4",
+      "title": "USP oferece curso gratuito de modelagem 3D para jogos eletrônicos",
+      "description": "Apresentar uma introdução à modelagem 3D para o desenvolvimento de jogos eletrônicos. Esse é o objetivo de um curso gratuito que acontece no próximo domingo, 15 de abril, no Instituto de Ciências Matemáticas e de Computação (ICMC) da USP, em São Carlos.A iniciativa atende a uma demanda crescente por especialização na área de desenvolvimento de games, que vem ganhando posição de destaque no Brasil. Durante o curso, o aluno aprenderá conceitos e técnicas para a modelagem 3D de jogos, utilizando a ferramenta aberta e gratuita Blender. Por meio dessa ferramenta, os participantes poderão aplicar de forma prática os conhecimentos adquiridos durante o curso. Por isso, solicita-se aos participantes que tragam notebook, com o software Blender já instalado. Voltado a todos os interessados, com qualquer tipo de formação, o curso acontecerá das 14 às 18 horas na sala 4-001, no bloco 4 do ICMC. Há 30 vagas disponíveis e as inscrições podem ser realizadas até quinta-feira, dia 12, ou enquanto houver vagas. Para participar, é preciso acessar o Sistema Apolo da USP neste link: icmc.usp.br/e/66a05.",
+      "photoCover": "https://4.bp.blogspot.com/-hRQnhkF0c8M/WsvASS4J26I/AAAAAAAAJRQ/3POeA8tA-UMQGFMqj9ubuJkfGcAmbGM3QCLcBGAs/s1600/features_model.jpg"
+     }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,25 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {dataFake} from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
-export class ContentComponent implements OnInit{
-
-  photoCover:string = "https://user-images.githubusercontent.com/70382532/138322189-2db8df52-9dcb-40a0-88a8-c365466bd33d.gif"
-  contentTitle:string = "MINHA NÓTICIA"
-  contentDescription = "Hello World!"
+export class ContentComponent implements OnInit {
+  photoCover:string = ""
+  contentTitle:string = ""
+  contentDescription:string = ""
+  private id:string | null = "0"
 
   constructor(
-    private route: ActivatedRoute
-  ){}
+    private route:ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe( value => console.log(value.get("id"))
-      
+    this.route.paramMap.subscribe( value =>
+     this.id = value.get("id")
     )
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id == id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
   }
 
 }
